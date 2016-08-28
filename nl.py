e1c6377cd36029ff048fb14d120cea63133a4af3
rs3guy <rs3guy@gmail.com>
2016-08-27 19:17:54 -0500

    Started work on data-logging

--- nl.py
+++ nl.py
@@ -8,8 +8,19 @@ s.listen(1)
 conn, address = s.accept()
 print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
 
+<<<<<<< HEAD
+while Try:
+    data = conn.recv(1024)
+    if not data: break
+	data_type = data[:data.index(':')]
+	print(data_type)
+    if data[:].encode("UTF-8")
+
+conn.close()
+=======
 while 1:
     data = conn.recv(1024)
     if not data: break
     conn.sendall(data)
 conn.close()
+>>>>>>> 47dd6b825f672ef01dc20a700522ea203259cdbb
