e701025f65296aad78c584898fb9cec6c402f5cd
rs3guy <rs3guy@gmail.com>
2016-10-02 03:29:13 -0500

    Add files via upload

--- nl-client.py
+++ nl-client.py
@@ -2,13 +2,12 @@ import os
 import socket
 import subprocess
 
-HOST = '192.168.1.14'    # The remote host
-PORT = 8008              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+cmd = "data"
 
-log = "log:TEST_PROG|This is a log file, baby!"
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host = ''
+port = 8080
 
-s.send(log)
-s.close()
-
+s.connect((host,port))
+print(s.getpeername())
+#s.send(cmd.encode())
--- nl.py
+++ nl.py
@@ -1,18 +1,50 @@
 import socket
-
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 8008               # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, address = s.accept()
-print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
-
-while Try:
-    data = conn.recv(1024)
-    if not data: break
-    data_type = data[:data.index(':')]
-    print(data_type)
+import sys
 
 
-conn.close()
+# Create socket (allows two computers to connect)
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 8080
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+
+# Bind socket to port (the host and port the communication will take place) and wait for connection from client
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+        print("Binding socket to port: " + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+        socket_bind()
+
+
+# Establish connection with client (socket must be listening for them)
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
+
+def socket_recv():
+    while True:
+        data = s.recv(1024)
+        if  len(data) > 0:
+            print(data.decode("utf-8"))
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+    socket_recv()
+
+
+main()
